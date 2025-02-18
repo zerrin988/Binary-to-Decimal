48e7bf4d80d0a7896303208f5a6c18b7a6abdcd8
Zerrin ul haq. Siya <134535520+zerrin988@users.noreply.github.com>
2025-02-19 00:20:15 +0100

    Update binary_to_decimal.c

--- binary_to_decimal.c
+++ binary_to_decimal.c
@@ -2,24 +2,24 @@
 #include <math.h>
 int main()
 {
-  int hi=0;
+  int hi = 0;
   int trial[5];
   int a;
   int res;
   int deci = 0;
   int lenght = 4;
   int i;
-  int binaryNum[4]={1,1,1,0}; //Change input manually
-  int changer =sizeof(binaryNum) / sizeof(binaryNum[0]);
-    for(a = changer;a > 0;a--)
+  int binaryNum[4] = {1,1,1,0}; //Change input manually
+  int changer = sizeof(binaryNum) / sizeof(binaryNum[0]);
+    for(a = changer; a > 0; a--)
     {
       trial[hi] = binaryNum[a-1];
       hi ++;
     }
-    for(i=0;i<lenght;i++)
+    for(i = 0; i < lenght; i++)
     {
-      res=pow(2,i);
-      deci += trial[i]*res;
+      res = pow(2, i);
+      deci += trial[i] * res;
     }
-  printf("%d is the answer",deci);   
+  printf("%d is the answer", deci);   
 }
